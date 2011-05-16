2318073ccedcbd4a653fb85e86e2e5f79341f2fd
George Phillips <Georgepaulphillips@gmail.com>
2011-05-16 16:07:33 +1200

    got file reading working

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 using namespace std;
 
-int main(void) {
-   cout << "Hello World\n";
+int main(int argc, char* argv[]) {
+   string line;
+   ifstream script;   if (argc != 2) {
+      cout << "Please enter one command line argument as the path to the file\n";
+      return EXIT_FAILURE;
+   }
+   
+   script.open(argv[1]);
+   if (script.is_open()) {
+      cout << "OPENED " << argv[1] << " \n"; 
+      while (script.good()) {
+            getline (script,line);
+            cout << line << endl;
+      }
+      script.close();
+   } else {
+      cout << "Unable to open " << argv[1] << "\n";
+   }
    return EXIT_SUCCESS;
 }
