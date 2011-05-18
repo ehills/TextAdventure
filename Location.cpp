78ce4ff61e35a6acf3e23930c8bb187fe58e511b
Edward Hills <ehills666@gmail.com>
2011-05-18 16:21:01 +1200

    Commented Location.cpp

--- Location.cpp
+++ Location.cpp
@@ -17,25 +17,29 @@ Location::Location(string name, string description, map<string, Item> items) {
     this->items = items;
 }
 
-
-// Details
-
+/* Returns the name of this location */
 string Location::getName(void) {
     return this->name;
 }
 
+/* Sets the name of this Location */
 void Location::setName(string name) {
     this->name = name;
 }
 
+/* Returns the description of this room */
 string Location::getDescription(void) {
     return this->description;
 }
 
+/* Sets the description of this room */
 void Location::setDescription(string description) {
     this->description = description;
 }
 
+/* Prints out the description plus the objects that are currently in this
+ * location to the user
+ */
 void Location::printRoom(void) {
    cout << getDescription() << listItems();
 }
@@ -45,7 +49,7 @@ void Location::addItem(string item_name, Item item) {
     this->items.insert(pair<string, Item > (item_name, item));
 }
 
-/* Adds an item to the room */
+/* Checks to see if item is in this location */
 bool Location::hasItem(string item_name) {
     return this->items.count(item_name) != 0;
 }
@@ -55,7 +59,7 @@ void Location::removeItem(string item_name) {
     this->items.erase(item_name);
 }
 
-/* Iterates through all items in the room and prints there description */
+/* Returns the description of each item in this location */
 string Location::listItems(void) {
    string the_items ="";
    map<string,Item>::iterator it;
@@ -65,40 +69,48 @@ string Location::listItems(void) {
    return the_items;
 }
 
-/* Destructor */
-Location::~Location(void) {
-}
-
-// Directions
-
+/* Returns the location pointed to by north */
 Location* Location::getNorth(void) {
     return this->north;
 }
 
+/* Returns the location pointed to by south */
 Location* Location::getSouth(void) {
     return this->south;
 }
 
+/* Returns the location pointed to by east */
 Location* Location::getEast(void) {
     return this->east;
 }
 
+/* Returns the location pointed to by west */
 Location* Location::getWest(void) {
     return this->west;
 }
 
+/* Sets the location pointed to by north */
 void Location::setNorth(Location* location) {
     this->north = location;
 }
 
+/* Sets the location pointed to by south */
 void Location::setSouth(Location* location) {
     this->south = location;
 }
 
+/* Sets the location pointed to by east */
 void Location::setEast(Location* location) {
     this->east = location;
 }
 
+/* Sets the location pointed to by west */
 void Location::setWest(Location* location) {
     this->west = location;
 }
+
+/* Destructor */
+Location::~Location(void) {
+}
+
+
