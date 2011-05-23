65a773f0d6366165fd947797499f2b139692be52
Samuel Garner <garnersamuel@hotmail.com>
2011-05-23 17:26:13 +1200

    game file changed called haunted_manison_broken

--- output/compiled_game.cpp
+++ output/compiled_game.cpp
@@ -20,7 +20,7 @@ Location diningRoom("The dining room", "An old dusty dinning room, complete with
 Location entranceHall("The entrance hall", "You find yourself in a dimly lit expansive entrance hall illuminated only by the faint moonlight filtering through the dusty windows. You can only see darkness to the north and doorways to the east and west");
 Location garden("The overgrown garden", "An overgrown garden in front of an old, spooky mansion");
 Location masterBedroom("The master bedroom", "You are surrounded in darkness. You cannot see anything but can hear strange noises close by. A cold chill rushes over you. To the east is the Entrance hall where you just entered from");
-Location secretHall("The secret hall", "A long narrow hallway expands out in front of you, you see an opening at the end of the hall  which you can feel a dark forbidding presence coming from. You notice openings to the south and north");
+Location secretHall("The secret hall", "A long narrow hallway expands out in front of you, you see an opening at the end of the hall which you can feel a dark forbidding presence coming from. You notice openings to the south and north");
 Location tortureRoom("The torture room", "You enter a cold stone room and instantly feel terrified. You see pentagrams carved into the stone floors which look to be covered in fresh blood. A hooded apparition materializes in front of you, wielding a menacing scythe. You can see your death is it's eyes");
 
 diningRoom.setWest(&entranceHall);
@@ -31,11 +31,11 @@ masterBedroom.setEast(&entranceHall);
 secretHall.setNorth(&tortureRoom);
 secretHall.setSouth(&entranceHall);
 tortureRoom.setSouth(&secretHall);
-Location compiler_inventory("Inventory", "Description of the inventory");Player* andy = new Player();andy->setLocation(&garden);andy->setInventory(&compiler_inventory);andy->setMaxItems(4);Item lamp("A lamp", "A rusty old lamp but looks like it still works");
-diningRoom.addItem("A lamp", &lamp);
+Location compiler_inventory("Inventory", "Description of the inventory");Player* andy = new Player();andy->setLocation(&garden);andy->setInventory(&compiler_inventory);andy->setMaxItems(4);Item lamp("lamp", "A rusty old lamp but looks like it still works");
+diningRoom.addItem("lamp", &lamp);
 lamp.setLocation(&diningRoom);
-Item mirror("A mirror", "You see your handsome, confident reflection in the mirror. This mirror could be useful..");
-masterBedroom.addItem("A mirror", &mirror);
+Item mirror("mirror", "You see your handsome, confident reflection in the mirror. This mirror could be useful..");
+masterBedroom.addItem("mirror", &mirror);
 mirror.setLocation(&masterBedroom);
 cout << WELCOME_MESSAGE << endl;prompt = "\n>>> ";cout << andy->getLocation()->getDescription() << endl;while (true) {   main_loop:   cout << prompt;   getline(cin, command);   istringstream word(command);   verb = "";   noun = "";   count = 0;   while (word) {      if (count > 2) {           cout << "I do not understand your command. Enter 2 words at most, a verb followed by a noun" << endl;           goto main_loop;      }      if (count == 0) {           word >> verb;      } else {           word >> noun;      }      count++;   }    if (command == QUIT_GAME) {
         quit_loop:
@@ -107,7 +107,7 @@ cout << andy->getLocation()->listItems() << endl;
 goto main_loop;}
 cout << "I don't know how to " << verb << " here" << endl;
 } else {
-if (andy->getLocation()->hasItem("A lamp") || andy->getInventory()->hasItem("A lamp")) {
+if (andy->getLocation()->hasItem("lamp") || andy->getInventory()->hasItem("lamp")) {
 if (verb == "drop") {
 if (andy->getInventory()->hasItem(lamp.getName())) {
 andy->getLocation()->addItem(lamp.getName(), &lamp);
@@ -121,19 +121,25 @@ cout << lamp.getName() << endl;
 goto main_loop;}
 if (verb == "light") {
 entranceHall.setDescription("You can now make out a door to the north");entranceHall.setNorth(&secretHall);
-masterBedroom.setDescription("You can now make out a mirror in the far corner of the room");goto main_loop;}
+masterBedroom.setDescription("You can now make out a mirror in the far corner of the room");cout << "You fiddle around with the old lamp and manage to get it working";
+} else {cout << "You don't have that item";
+}
+goto main_loop;}
 if (verb == "off") {
 entranceHall.setDescription("You find yourself in a dimly lit expansive  entrance hall, only illuminated by the faint moonlight filtering through the dusty windows. You can only make out darkness to the north");entranceHall.setNorth(NULL);
-masterBedroom.setDescription("You are surrounded in darkness. You cannot see anything but can hear strange noises close by. A cold chill rushes over you");goto main_loop;}
+masterBedroom.setDescription("You are surrounded in darkness. You cannot see anything but can hear strange noises close by. A cold chill rushes over you");cout << "You switch off the old lamp";
+} else {cout << "You don't have that item";
+}
+goto main_loop;}
 if (verb == "pickup") {
 if (andy->canCarry()) {
 andy->getInventory()->addItem(lamp.getName(), &lamp);
 } else {cout << "You are carrying too much already";
 }
 goto main_loop;}
-cout << "Sorry you can not " << verb << "on" << "A lamp" << endl;
+cout << "Sorry you can not " << verb << "on" << "lamp" << endl;
 goto main_loop;
-}if (andy->getLocation()->hasItem("A mirror") || andy->getInventory()->hasItem("A mirror")) {
+}if (andy->getLocation()->hasItem("mirror") || andy->getInventory()->hasItem("mirror")) {
 if (verb == "drop") {
 if (andy->getInventory()->hasItem(mirror.getName())) {
 andy->getLocation()->addItem(mirror.getName(), &mirror);
@@ -158,7 +164,7 @@ break;
 } else {cout << "You admire yourself in the mirror.";
 }
 goto main_loop;}
-cout << "Sorry you can not " << verb << "on" << "A mirror" << endl;
+cout << "Sorry you can not " << verb << "on" << "mirror" << endl;
 goto main_loop;
 }cout << "I can't find a " << noun << " here" << endl;
 }
