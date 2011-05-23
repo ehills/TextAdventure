5b328e59bfcf1672f9da1dc390128047290fd194
Edward Hills <ehills666@gmail.com>
2011-05-23 13:30:26 +1200

    Added error messages to the parser to be printed to the user in compile time

--- constants.h
+++ constants.h
@@ -2,15 +2,23 @@
  * File: Constants.h
  * Name: Constants
  * Date: May 19 2011
- * Description: Some constants for IO
+ * Description: Some constants for parseing the users game file
  */
 #ifndef CONSTANTS_H_
 #define CONSTATNS_H_
 
 // ERRORS
 #define ERROR_WRONG_ARGUMENTS "Please enter one command line argument as the path to the file\n"
-#define ERROR_BAD_FILE "The file specified was not able to be opened\n"
-#define ERROR_TEMP_FILE_NOT_OPEN "The temporary file was unavailable\n"
-#define ERROR_UNCLOSED_COMMENT "There is an unclosed multiline comment\n"
+#define ERROR_BAD_FILE "The file specified was not able to be opened.\n"
+#define ERROR_TEMP_FILE_NOT_OPEN "The temporary file was unavailable.\n"
+#define ERROR_UNCLOSED_COMMENT "There is an unclosed multiline comment.\n"
+#define BAD_LOCATION "Cannot find your location."
+#define NO_LOCATION_NAME "Your location must have a name."
+#define NO_ITEM "Cannot find item."
+#define NO_LOCATION_DESCRIPTION "Your location does not have a description."
+#define NO_JOINED_LOCATION "You are trying to join to a room that does not exist."
+#define NO_ITEM_NAME "Your item does not have a name."
+#define NO_ITEM_DESCRIPTION "Your item does not have a description."
+#define NO_ITEM_LOCATION "Your item is not associated with a location."
 
 #endif
