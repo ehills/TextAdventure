188f101fd59f51b002fbc5126c721e2a03c6af42
Samuel Garner <garnersamuel@hotmail.com>
2011-08-13 21:21:24 +1200

    added some error catching in compiler.cc, need to do some more

--- constants.h
+++ constants.h
@@ -1,4 +1,4 @@
-/* Description: Some constants for parseing the users game file */
+/* Description: Some constants for parsing the users game file */
 
 #ifndef CONSTANTS_H_
 #define CONSTATNS_H_
@@ -7,7 +7,7 @@
 #define ERROR_WRONG_ARGUMENTS "Please enter one command line argument as the path to the file\n"
 #define ERROR_BAD_FILE "The file specified was not able to be opened.\n"
 #define ERROR_TEMP_FILE_NOT_OPEN "The temporary file was unavailable.\n"
-#define ERROR_UNCLOSED_COMMENT "There is an unclosed multiline comment.\n"
+#define ERROR_UNCLOSED_COMMENT "There is an unclosed multiple line comment.\n"
 #define BAD_LOCATION "Cannot find your location."
 #define NO_LOCATION_NAME "Your location must have a name."
 #define NO_ITEM "Cannot find item."
@@ -17,12 +17,12 @@
 #define NO_ITEM_DESCRIPTION "Your item does not have a description."
 #define NO_ITEM_LOCATION "Your item is not associated with a location."
 #define UNKNOWN_COMMAND "Unreadable/Unknown Command: \""
-#define UNKNOWN_PRINT_STATEMENT "Unknown/Illformed Print Statement."
+#define UNKNOWN_PRINT_STATEMENT "Unknown/ill-formed Print Statement."
 #define INCOMPLETE_EXPRESSION "Your expression is incomplete."
 #define BAD_CARRIABLE_EXPRESSION "Your carry expression is wrong."
 #define BAD_INLOCATION "Your inLocation statement is incorrect."
 #define BAD_BOOLEAN "Your boolean (true/false) expression is incorrect."
-#define BAD_BRACES "Your braces {, } are not sentactically correct."
+#define BAD_BRACES "Your braces {, } are not syntactically correct."
 #define ONLY_ONE_LOCATION "You need more than one Location."
 #define NO_LOCATIONS "You do not have any Locations."
 #define PARSER_FAIL "The Parser did not complete without errors."
